4510258ec87aef9283940b991a8c3ed3a10b4f0b
Sanjeev <jagansanjeev2007@gmail.com>
2026-09-20 11:20:16 +0530

    updated code class component and functional component

---
+++ src/pages/Demo1.jsx
@@ -0,0 +1,22 @@
+function Demo1()
+{
+    let a=10,b=20.5
+    let name="klu"
+    let status=true
+
+    return (
+        <div>
+        <h3>Funtional Component Demo</h3>
+        <p>Demo React App</p>
+        <font color="green" size="+2">Front end Application Development</font>
+
+        <p>{a}</p>
+        <p>{b}</p>
+        <p>{status.toString()}</p>
+
+
+        </div>
+    )
+}
+
+export default Demo1
---
+++ src/pages/Demo3.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Demo3 extends React.Component
+{
+    render () {
+        return (
+            <div>
+            <h3>Class Component Demo</h3>
+            <p>FSAD</p>
+            </div>
+        )
+    }
+}
+
+export default Demo3
